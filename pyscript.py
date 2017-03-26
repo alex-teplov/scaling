560c7702dbe3bec5cae0ff088d002fc2e9fec82c
Alex Teplov <alex-teplov@yandex.ru>
2017-03-26 15:10:14 +0300

    Initial commit for scaling experiments work Implementation of scaling experiment classes, merged data class, and data extracting and merging

---
+++ pyscript.py
@@ -0,0 +1,148 @@
+
+import os
+from scaling_experiment import Experiment, MergedExperiment
+import itertools
+
+log_directory = '/Users/teplov/Dropbox/НИВЦ/Upsala/Logs from Jonathan dence/'
+log_file = 'fmm3d_p1_10000k_l3_t8_206971.log'
+log_64 = 'fmm3d_p64_10000k_l3_t8_206997.log'
+
+log_64_2 = 'fmm3d_p64_10000k_l3_t8_206998.log'
+log_check = 'fmm3d_p1_10000k_l3_t8_206971.log'
+full_log_path = log_directory+log_64
+# print (full_log_path)
+# extract_params(full_log_path)
+# dir_extract(log_directory,".log")
+# check_file( log_directory+'fmm3d_p4_10000k_l4_t8_207009.log')
+# 
+def dir_extract(dir_path, filter_value):
+	fileslist = os.listdir(dir_path)
+	datafiles = filter(lambda x: x.endswith(filter_value), fileslist)
+	dir_data = []
+	for file in datafiles:
+		full_file_name = dir_path+file
+		print (file)
+		
+		dir_data.append(Experiment(full_file_name))
+	return dir_data
+		# if check_file(full_file_name) ==0:
+		# 	print (extract_params(full_file_name))
+		# 	print (log_max_time(full_file_name))
+# 
+
+
+def collect_params(launch_set):
+	params_names = set()
+	params = {}
+	for experiment in launch_set:
+		params_names = params_names.union(set(experiment.launch_parameters.keys()))
+		for param in experiment.launch_parameters.keys():
+			if params.get(param) == None:
+				params[param] = set()
+			params[param].add(experiment.launch_parameters[param])
+			params[param] = set(params[param])
+
+			
+	print(params_names)
+	return params
+
+def merge_experiments(launch_set):
+	merged_set = set()
+	for experiment in launch_set:
+		# print(experiment.launch_parameters, experiment.data_extract)
+		
+		# print(experiment.source_log)
+		if experiment.source_log not in merged_set:
+			pass
+	# print(merged_set)
+
+
+
+a = Experiment(log_directory+log_64)
+a_2 = Experiment(log_directory+log_64_2)
+a_3 = Experiment(log_directory+log_file)
+# print (a.launch_parameters, a.data_extract)
+
+data = dir_extract(log_directory, '.log')
+
+print ("\n Total number of parsed logs: {0}".format(len(data)))
+
+# print('\n Comments in the data:')
+# for experiment in data:
+# 	if experiment.comment =='':
+# 		pass
+# 	else:
+# 		print(experiment.source_log, experiment.comment)
+
+# del a
+
+# print( collect_params(data))
+
+merge_experiments(data)
+
+# b = MergedExperiment(a)
+# print(b.data_extract)
+# c = MergedExperiment(a_2)
+# print(c.data_extract)
+# print(b.merge(a_2))
+# print(b.merge(a_3))
+# # print(b.launch_data)
+# print(b.data_extract, b.source_log)
+# print(c.data_extract, c.source_log)
+# del b,c
+
+merged_data = []
+experiment_set = data[0:]
+i=0
+print ("Number of logs : {0}".format(len(experiment_set)))
+for experiment in experiment_set:
+	print (experiment.source_log)
+	if experiment.comment:
+		continue
+
+	for merged_experiment in reversed(merged_data):
+		if merged_experiment.merge(experiment):
+			i+=1
+			continue
+		# print('0 Length of merged_data : {0}'.format(len(merged_data)))
+		# print('0 Length of current source_log: {0}'.format(len(merged_experiment.source_log)))
+		# print('0 Current data_extract : {0}'.format(merged_experiment.data_extract))
+		i+=1
+		break
+
+	else:
+		# print('Else')
+		merged = MergedExperiment(experiment)
+		i+=1
+		# print('1 Creating new entiry')
+		# print('1 Length of merged_data : {0}'.format(len(merged_data)))
+		# print('1 Length of current source_log: {0}'.format(len(merged.source_log)))
+		# print('1 Current data_extract : {0}'.format(merged.data_extract))
+
+		# print(merged.source_log)
+		merged_data.append(merged)
+
+
+
+
+
+print('Number of iterations :{0}'.format(i))
+print('=======Total data========')
+print('merged_data length : {0}'.format(len(merged_data)))
+for data in merged_data:
+
+	print('4 Length of current source_log: {0}'.format(len(data.source_log)))
+	print('4 Params are : {0}'.format(data.launch_parameters))
+	for log in data.source_log:
+		print('      {0}'.format(log)) #print('4 Length of current source_log: {0}'.format(data.source_log))
+	print('4 Current data_extract : {0}'.format(data.data_extract))
+
+# print('Hello')
+
+
+
+		
+
+
+
+
---
+++ scaling_experiment.py
@@ -0,0 +1,118 @@
+
+import os
+
+class Experiment:
+	# source_log = ''
+	# launch_parameters = {}
+	# launch_data = {}
+	# data_extract = {}
+	# comment = ''
+
+	def __init__(self, log_path):
+		self.source_log = log_path
+		self.launch_parameters = self.extract_params(log_path)
+		self.comment = ''
+		if self.check_file(log_path):
+			print("Not a log file")
+			self.comment = 'Error in datafile import. Required rerun on the params {0}'.format(self.launch_parameters)
+		else:
+			self.launch_data = self.readlog(log_path)
+			self.data_extract = self.log_max_time(log_path)
+			# print ("Experiment {0} inited".format(self.source_log))
+	
+	# def print(self):
+	# 	for self.__dict__
+
+
+	def check_file(self, logfile):
+		# print('Check method')
+		filedata = open(logfile,'r')
+		if (filedata.read(32))=='--------------------------------':
+			print("Error file")
+			filedata.close()
+			return 1
+		else:
+			filedata.close()
+			return 0
+
+	def extract_params(self, logfile):
+		# print('Extract method')
+		linewords = logfile.split('/')
+		params = linewords[-1].split('_')
+		# print (params)
+		params_data = {}
+		params_data['Procs'] = int(params[1][1:])
+		params_data['Problem_size'] = int(params[2][:-1])*1000
+		params_data['Levels'] = int(params[3][1:])
+		params_data['Tolerance'] = int(params[4][1:])
+		return params_data
+
+	def readlog(self, logfile):
+		# print('Readlog method')
+		filedata = open(logfile,"r")
+		# print (filedata.readlines())
+		log_data = {}
+		for line in filedata.readlines():
+			# print (line)
+			if line == "" or line =='\n' or line.find(":") ==-1:
+				# print ("Line Skiped")
+				continue
+			# print ('Line is : " {0} "'.format(line))
+			rank_number = int(line.split(":")[0].split(' ')[1][:-1])
+			# print('Rank is " {0} "'.format(rank_number))
+			step_number = line.split(":")[0].split(' ')[-1]
+			# print ('Step is: " {0} "'.format(step_number))
+			step_time = float(line.split(":")[1])
+			# print ('Step time is: " {0} "'.format(step_time))
+			# print (log_data.get(step_number))
+			if log_data.get(step_number) == None:
+				log_data[step_number] = {}
+			log_data[step_number][rank_number] = step_time
+			# print (log_data.get(step_number))
+	
+		# print(log_data)
+		filedata.close()
+		return log_data
+	def log_max_time(self, logfile):
+		log_data = self.readlog(logfile)
+		log_max_time = {}
+		# print(log_data)
+		# print(log_data.keys())
+		for key in log_data.keys():
+				log_max_time[key] = max(log_data[key].values())
+		# print(log_max_time)
+		return log_max_time
+
+class MergedExperiment(Experiment):
+	# source_log = set()
+	# comment = set()
+
+	def __init__(self, experiment_obj):
+		self.source_log = {experiment_obj.source_log,} 
+		self.launch_parameters = experiment_obj.launch_parameters
+		self.launch_data = {}
+		self.data_extract = {}
+		self.comment = set()
+		for key in experiment_obj.launch_data.keys():
+			self.launch_data[key] = [experiment_obj.launch_data[key]]
+		for key in experiment_obj.data_extract.keys():
+			self.data_extract[key] = [experiment_obj.data_extract[key]]
+		# self.data_extract = experiment_obj.data_extract
+		self.comment.add(experiment_obj.comment)
+
+	def merge(self, experiment_obj):
+		if self.launch_parameters == experiment_obj.launch_parameters:
+			print('Merge is possible')
+			self.source_log.add(experiment_obj.source_log)
+			for key in experiment_obj.launch_data.keys():
+				self.launch_data[key].append(experiment_obj.launch_data[key])
+			for key in experiment_obj.data_extract.keys():
+
+				self.data_extract[key].append(experiment_obj.data_extract[key])
+			return 0
+
+		else:
+			print('Error: Merge is impossible')
+			return 1
+
+
