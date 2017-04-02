7bcb27958f69dfc3f94dc7024c1a9cab74c80c38
Alex Teplov <alex-teplov@yandex.ru>
2017-04-03 01:25:01 +0300

    Implemented the output of app part timings in files Implemented the correct output of the files for scaling plotting

--- pyscript.py
+++ pyscript.py
@@ -129,62 +129,70 @@ for experiment in experiment_set:
 
 
 print('Number of iterations :{0}'.format(i))
-print('=======Total data========')
-print('merged_data length : {0}'.format(len(merged_data)))
-for data in merged_data:
+# print('=======Total data========')
+# print('merged_data length : {0}'.format(len(merged_data)))
+# for data in merged_data:
 
-	print('4 Length of current source_log: {0}'.format(len(data.source_log)))
-	print('4 Params are : {0}'.format(data.launch_parameters))
-	for log in data.source_log:
-		print('      {0}'.format(log)) #print('4 Length of current source_log: {0}'.format(data.source_log))
-	print('4 Current data_extract : {0}'.format(data.data_extract))
-
-
-print('\n========Extraction of data==========')
-for data in merged_data:
-	print ('Parameters: {0}'.format(data.launch_parameters))
-	for app_part in data.data_extract:
-		print('     {0} : {1}'.format(app_part, numpy.percentile(data.data_extract[app_part],75)))
+# 	print('4 Length of current source_log: {0}'.format(len(data.source_log)))
+# 	print('4 Params are : {0}'.format(data.launch_parameters))
+# 	for log in data.source_log:
+# 		print('      {0}'.format(log)) #print('4 Length of current source_log: {0}'.format(data.source_log))
+# 	print('4 Current data_extract : {0}'.format(data.data_extract))
 
 
-print('=========Parts of the application===========')
+# print('\n========Extraction of data==========')
+# for data in merged_data:
+# 	print ('Parameters: {0}'.format(data.launch_parameters))
+# 	for app_part in data.data_extract:
+# 		print('     {0} : {1}'.format(app_part, numpy.percentile(data.data_extract[app_part],75)))
+
+
+# print('=========Parts of the application===========')
 parts_array = {}
 data_set =  merged_data[0:]
 for data in data_set:
-	print('Parsing merge data:')
-	print ('Parameters: {0}'.format(data.launch_parameters))
+	# print('Parsing merge data:')
+	# print ('Parameters: {0}'.format(data.launch_parameters))
 	for app_part in data.data_extract:
 		
-		print('     {0} : {1}'.format(app_part, numpy.percentile(data.data_extract[app_part],75)))
+		# print('     {0} : {1}'.format(app_part, numpy.percentile(data.data_extract[app_part],75)))
 		if parts_array.get(app_part):
-			print('Some parts here')
+			
 			parts_array[app_part].add(data.launch_parameters,numpy.percentile(data.data_extract[app_part],75))
 
 		else: 
-			print(' Creating App part with {0}, {1}, {2}'.format(app_part, data.launch_parameters,numpy.percentile(data.data_extract[app_part],75)))
+			# print(' Creating App part with {0}, {1}, {2}'.format(app_part, data.launch_parameters,numpy.percentile(data.data_extract[app_part],75)))
 			# part_entity = Application_part()
 			application_part_entity = Application_part(app_part, data.launch_parameters, numpy.percentile(data.data_extract[app_part],75))
-			print(' Entity name : {0}'.format(application_part_entity.part_name))
+			# print(' Entity name : {0}'.format(application_part_entity.part_name))
 			parts_array[application_part_entity.part_name] = application_part_entity
 
-	# for part in parts_array:
-	# 	print ('a')
-
-	# else: 
-	# 	print('c') 
-	# 	for app_part in data.data_extract:
-	# 		print(' Creating App part with {0}, {1}, {2}'.format(app_part, data.launch_parameters,numpy.percentile(data.data_extract[app_part],75)))
-	# 		# part_entity = Application_part()
-	# 		application_part_entity = Application_part(app_part, data.launch_parameters, numpy.percentile(data.data_extract[app_part],75))
-	# 		print(' Entity name : {0}'.format(application_part_entity.part_name))
-	# 		parts_array.append(application_part_entity)
 
 print ('Found parts: {0}'.format(len(parts_array)))
+# for part in parts_array:
+# 	print ('   {0}'.format(part))
+# 	print ('       {0}'.format(parts_array[part].part_name))
+# 	for timing in parts_array[part].param_timing:
+# 		print ('            {0} : {1}'.format(timing.params, timing.time))
+
+
+
+print(parts_array['Alloc'].part_name)
+# sorted(parts_array['Alloc'].param_timing)
+
 for part in parts_array:
-	print ('   {0}'.format(part))
-	print ('       {0}'.format(parts_array[part].part_name))
+	print(part)
+
+	f = open('{0}_timing.txt'.format(parts_array[part].part_name),'w')
 	for timing in parts_array[part].param_timing:
-		print ('            {0} : {1}'.format(timing.params, timing.time))
+	# print ('            {0} : {1}'.format(timing.params, timing.time))
+	
+	# print ('{0} {1} {2}'.format(timing.params['Procs'], timing.params['Levels'], timing.time))
+		f.write('{0} {1} {2}\n'.format(timing.params['Procs'], timing.params['Levels'], timing.time))
+
+	f.close()
+
+
 
 
 
